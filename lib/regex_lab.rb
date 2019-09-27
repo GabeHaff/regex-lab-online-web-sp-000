54c1be4da8ea70be3cbe75effd0f91d15e1177b1
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-27 21:46:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-word.match(/\b[uning])
+word.scan(/\b[uning])
 end
 
 def words_five_letters_long(text)
